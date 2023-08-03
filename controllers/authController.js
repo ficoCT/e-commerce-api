dd774d623340d9107163e91d6a26ecfb49d8345e
FICO\rafal <rafalficek082@gmail.com>
2023-08-03 20:22:42 +0200

    Register

---
+++ controllers/authController.js
@@ -0,0 +1,33 @@
+const User = require('../models/User');
+const { StatusCodes } = require('http-status-codes');
+const CustomError = require('../errors');
+
+const register = async (req, res) => {
+  const { email, name, password } = req.body;
+
+  const emailAlreadyExists = await User.findOne({ email });
+  if (emailAlreadyExists) {
+    throw new CustomError.BadRequestError('Email already exists');
+  }
+
+  // first registered user is an admin
+  const isFirstAccount = (await User.countDocuments({})) === 0;
+  const role = isFirstAccount ? 'admin' : 'user';
+
+  const user = await User.create({ name, email, password, role });
+  const tokenUser = createTokenUser(user);
+  attachCookiesToResponse({ res, user: tokenUser });
+  res.status(StatusCodes.CREATED).json({ user: tokenUser });
+};
+const login = async (req, res) => {
+  res.send('login');
+};
+const logout = async (req, res) => {
+  res.send('logout');
+};
+
+module.exports = {
+  register,
+  login,
+  logout,
+};
